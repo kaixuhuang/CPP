ce3faaa5b404d840a74632ad1c1edc183f97c465
kaixu huang <1320048219@qq.com>
2022-06-28 12:44:09 +0800

    翻转链表

--- binarytree/reversal.cpp
+++ binarytree/reversal.cpp
@@ -11,7 +11,7 @@ struct TreeNode {
 	TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
 	TreeNode(int x, TreeNode* left, TreeNode* right) : val(x), left(left), right(right) {}
 
-};
+}; 
 class Solution {
 public:
 	//226-ตÝน้
